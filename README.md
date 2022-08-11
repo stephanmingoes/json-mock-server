ee5e74f0591ef6dcb383f262c05e88f83ff871a0
stephanminges <stephanminges@gmail.com>
2022-08-10 21:35:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# JSON-MOCK-SERVER
+
+Simple tool for turning JSON file into a fake REST API in seconds.
+
+## Steps
+
+- Create a github repository. Eg. Test
+- Create a JSON file in root of repository
+- The endpoint for your data is now https://www.json-faker-server.herokuapp.com/<github-username>/<repo-name>/<name-of-json-file.json>
---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import axios from "axios";
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is live",
+  });
+});
+
+app.get("/:username/:reponame/:jsonfile", async (req, res) => {
+  const { username, reponame, jsonfile } = req.params;
+  try {
+    const data = await axios.get(
+      `https://api.github.com/repos/${username}/${reponame}/contents/${jsonfile}`
+    );
+
+    const jsonContent = JSON.parse(
+      Buffer.from(data.data.content, "base64").toString("ascii")
+    );
+    res.json(jsonContent);
+  } catch (error) {
+    res.status(400).json({
+      message:
+        "Something went wrong, ensure that the *JSON* file is in the root of the repository.",
+    });
+    console.log(error);
+  }
+});
+
+app.listen(port, () =>
+  console.log(`Express is listening at http://localhost:${port}`)
+);
